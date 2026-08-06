71f1389376c15b6f4c831c55e617925af53e90f5
veronicabassey290-eng <veronicabassey290@gmail.com>
2026-08-06 04:35:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://scrjczkluiuwitxbzeud.supabase.co";
 
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = "sb_publishable_83A-4E-xP50UwITDwZ2McQ_oB5hbbP7";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
